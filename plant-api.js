5d17c770ca1ac1a663a654317a269b70589a735a
Jenny <jennyliarch@gmail.com>
2024-10-06 23:43:22 -0400

    updated javascript file

--- plant-api.js
+++ plant-api.js
@@ -3,8 +3,26 @@
 // //code;
 // } im so confused..
 
-let fileInput = document.querySelector('.fileInput');
+//let fileInput = document.querySelector('.fileInput');
 
-function Upload () {
+//function Submit () {
     //code 
+//}
+
+function uploadFile() {
+    let fileInput = document.getElementById('fileInput');
+    let file = fileInput.files[0];
+
+    if (file) {
+        document.getElementById('status').textContent = 'Selected file: ${file.name}';
+    } else {
+        document.getElementById('status').textContent = 'No file selected.';
+    }
 }
+
+let myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", function(b) {
+    let name = alert('Please upload a photo');
+    console.log(name);
+});
