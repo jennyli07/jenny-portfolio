99abcb617fe41800410101efaa3b1758625b32d5
Jenny <jennyliarch@gmail.com>
2024-10-18 00:03:14 -0400

    updated code

---
+++ about.js
@@ -0,0 +1,14 @@
+document.getElementById("scrollToTop").addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
