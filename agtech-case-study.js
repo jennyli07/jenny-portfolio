4cce3610b0ae060598b2e117e2b5c30e4ae1ff03
Jenny <jennyliarch@gmail.com>
2024-10-22 18:46:53 -0400

    updated code

--- agtech-case-study.js
+++ agtech-case-study.js
@@ -12,3 +12,20 @@ function scrollToTop() {
         behavior: "smooth"
     });
 }
+
+// script.js
+
+window.onscroll = function() {
+    stickyNavbar();
+};
+
+let navbar = document.getElementById("navigation-bar");
+let sticky = navbar.offsetTop;
+
+function stickyNavbar() {
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
